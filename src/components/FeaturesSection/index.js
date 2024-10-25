33b1be0b9c03c9d1bb86d27d8535e33e4cf3c8b7
Soniya <boralakuntasoniya@gmail.com>
2024-10-25 09:19:26 +0000

    project

---
+++ src/components/FeaturesSection/index.js
@@ -0,0 +1,15 @@
+import Playtime from '../Playtime'
+import NewWaysToConnect from '../NewWaysToConnect'
+
+import './index.css'
+
+const FeaturesSection = props => {
+  return (
+    <div className="features-section-container">
+      <Playtime />
+      <NewWaysToConnect />
+    </div>
+  )
+}
+
+export default FeaturesSection
